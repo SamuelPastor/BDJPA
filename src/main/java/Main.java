e8ae71b0e40a3d7c408fede1c3a44308eee58461
Samuel <samupastor23@gmail.com>
2022-02-03 17:00:11 +0100

    Primer commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * spring-core de org.springframework, la última RELEASE
+         * • spring-beans
+         * • spring-context
+         * • spring-orm
+         * */
+    }
+}
